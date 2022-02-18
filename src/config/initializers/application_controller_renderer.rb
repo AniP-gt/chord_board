43d9bda9a1033c5cb8cc40cd9f8004ce1ead5546
AniP-gt <aniki.gt007@gmail.com>
2022-02-19 00:05:50 +0900

    マイページ、編集画面実装

--- src/config/initializers/application_controller_renderer.rb
+++ src/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b5b0b26f4d72a3354d635d0c462a6b95dffb4375171bbdc99651eec74e46e874fbb2caef3a76ef45eadaaa31f83e2289810902e2b7419d94eca00c55e472489c'
