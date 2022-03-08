075eb22b94b7aee1bc3ecf75799f3ebc8f446361
root <root@DESKTOP-M1AONNJ.localdomain>
2022-03-08 22:25:38 +0900

    user/show画面フォロー機能追加

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -5,14 +5,11 @@ class User < ApplicationRecord
   attachment :profile_image
   has_many :chords, dependent: :destroy
 
-  has_many :active_relationships, class_name: "Relationship",
-                                  foreign_key: "follower_id",
-                                  dependent: :destroy
-  has_many :following, through: :active_relationships, source: :followed
-  has_many :passive_relationships,  class_name: "Relationship",
-                                    foreign_key: "followed_id",
-                                    dependent: :destroy
-  has_many :followers, through: :passive_relationships, source: :follower
+  has_many :relationships, class_name: "Relationship",foreign_key: "follower_id",dependent: :destroy
+  has_many :follows, through: :relationships, source: :followed
+
+  has_many :reverse_of_relationships, class_name: "Relationship",foreign_key: "followed_id",dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
 
   with_options presence: true do
     validates :username
@@ -21,19 +18,17 @@ class User < ApplicationRecord
     validates :password_confirmation
   end
 
-  # ユーザーをフォローする
-  def follow(other_user)
-    following << other_user
-  end
-
-  # ユーザーをフォロー解除する
-  def unfollow(other_user)
-    active_relationships.find_by(followed_id: other_user.id).destroy
-  end
-
-  # 現在のユーザーがフォローしていたらtrueを返す
-  def following?(other_user)
-    following.include?(other_user)
-  end
+#　フォローする
+def follow(user_id)
+  relationships.create(followed_id: user_id)
+end
+# フォローを解除
+def unfollow(user_id)
+  relationships.find_by(followed_id: user_id).destroy
+end
+# フォローしているか判定
+def following?(user)
+  follows.include?(user)
+end
 
 end
--- src/config/routes.rb
+++ src/config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
   resources :users do
-    resources :relationships, only: [:create, :destroy]
-    get :following, on: :member
-    get :followers, on: :member
+    member do
+      get :follows, :followers
+    end
+    resource :relationships, only: [:create, :destroy]
   end
-  resources :chords ,only: [ :index, :new]
+  resources :chords
 end
