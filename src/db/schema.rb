d1881417e735f68dc48b007b87c503763325011a
root <root@DESKTOP-M1AONNJ.localdomain>
2022-03-30 14:19:38 +0900

    ローカル環境のメール認証機能実装

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -37,11 +37,16 @@ ActiveRecord::Schema.define(version: 2022_03_04_143117) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.string "username"
     t.text "profile"
     t.string "profile_image_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
